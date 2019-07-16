ed1c7993a95fb5b918e2939810d1d54f8573536d
Angelica Castillejos <angcast@mit.edu>
2019-07-16 16:40:48 -0700

    Angelica -2 - Fixed merge conflict part 2

--- app/src/main/java/com/fb/pearsapplication/MainActivity.java
+++ app/src/main/java/com/fb/pearsapplication/MainActivity.java
@@ -20,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-<<<<<<< HEAD
+        
         bottomNavigation = findViewById(R.id.bottomNavigation);
         setUpBottomNavigationView();
 
