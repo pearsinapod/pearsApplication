7504490be97b464021f85db5384cb81c81661c7e
Aarushi Agrawal <aarushi.agrawal77@gmail.com>
2019-07-16 17:36:56 -0700

    aarushi-2 commit login/signup complete

--- app/src/main/java/com/fb/pearsapplication/MainActivity.java
+++ app/src/main/java/com/fb/pearsapplication/MainActivity.java
@@ -24,8 +24,6 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation = findViewById(R.id.bottomNavigation);
         setUpBottomNavigationView();
 
-
-
     }
 
     public void setUpBottomNavigationView(){
