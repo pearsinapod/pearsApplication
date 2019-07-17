138df59dadc2e077b552b34d7722954e38ba717e
jaleel ismail <abdeljaleelismail@college.harvard.edu>
2019-07-16 17:49:23 -0700

    jaleel branch commit 2
    end of day commit

---
+++ app/src/main/java/com/fb/pearsapplication/fragments/groupDetailsFragment.java
@@ -0,0 +1,18 @@
+package com.fb.pearsapplication.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fb.pearsapplication.R;
+
+public class groupDetailsFragment extends Fragment {
+
+
+}
