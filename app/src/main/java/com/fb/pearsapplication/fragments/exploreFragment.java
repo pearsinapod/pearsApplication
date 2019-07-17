5319585b020df606a880c1870efe5d57ed6698bc
Angelica Castillejos <angcast@mit.edu>
2019-07-17 14:34:54 -0700

    Angelica- 8- Set up Recycler View and Explore Fragment class
    I set up the Recycler View for the explore fragment, and the group image and name are now displayed in explore fragment

--- app/src/main/java/com/fb/pearsapplication/fragments/exploreFragment.java
+++ app/src/main/java/com/fb/pearsapplication/fragments/exploreFragment.java
@@ -1,17 +1,67 @@
 package com.fb.pearsapplication.fragments;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.fb.pearsapplication.R;
 import com.fb.pearsapplication.adapters.exploreAdapter;
 import com.fb.pearsapplication.models.Group;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class exploreFragment extends Fragment {
 
     protected ArrayList<Group> exploreGroups;
     protected exploreAdapter eAdapter;
+    private RecyclerView rvExploreGroups;
 
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_explore, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        rvExploreGroups = view.findViewById(R.id.rvExploreGroups);
+        exploreGroups = new ArrayList<>();
+        eAdapter = new exploreAdapter(exploreGroups);
+        rvExploreGroups.setAdapter(eAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvExploreGroups.setLayoutManager(linearLayoutManager);
+        showPosts();
+    }
+
+    protected void showPosts(){
+        Group.Query posts = new Group.Query();
+        posts.getTop().withUser();
+        posts.addDescendingOrder(Group.KEY_CREATED_AT);
+        posts.findInBackground(new FindCallback<Group>() {
+            @Override
+            public void done(List<Group> objects, ParseException e) {
+                if (e == null){
+                    exploreGroups.addAll(objects);
+                    eAdapter.notifyDataSetChanged();
+                }
+                else{
+                    Log.d("Explore Fragment","Loading items failed");
+                }
+            }
+        });
+    }
 
 }
