c8713a9617701f53a1417b10c369d0d620f58757
Angelica Castillejos <angcast@mit.edu>
2019-08-05 16:54:35 -0700

    Angelica - 47 - Filtered groups and changed launcher image and name

--- app/src/main/java/com/fb/pearsapplication/fragments/groupDetailsFragment.java
+++ app/src/main/java/com/fb/pearsapplication/fragments/groupDetailsFragment.java
@@ -16,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.fb.pearsapplication.R;
 import com.fb.pearsapplication.models.Group;
 import com.fb.pearsapplication.models.GroupUserRelation;
@@ -71,15 +72,10 @@ public class groupDetailsFragment extends Fragment {
         ParseFile image = group.getGroupImage();
         if (image != null) {
             Glide.with(getContext()).load(image.getUrl()).into(ivGroupImage);
-        } else {
-            Glide.with(getContext()).load(R.drawable.people).into(ivGroupImage);
         }
-/*        else{
-            Glide.with(getContext()) //for some reason placeholder and error doesn't work for me: if it works for you lmk! - ang
-                    .load(R.drawable.group_search_placeholder)
-                    .apply(RequestOptions.circleCropTransform())
-                    .into(ivGroupImage);
-        }*/
+        else{
+            Glide.with(getContext()).load(R.drawable.group_search_placeholder).apply(RequestOptions.circleCropTransform()).into(ivGroupImage);
+        }
         if (group.getUsers() != null) {
             Integer size = group.getUsers().size();
             String sizeString = size.toString();
