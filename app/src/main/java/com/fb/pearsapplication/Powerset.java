92f4d3124db84567fd46061909f545968fbe1fe0
Angelica Castillejos <angcast@mit.edu>
2019-08-08 13:53:25 -0700

    Angelica - 54- Apiori Rough Draft
    Seems to be working. Just need to fix some changes on the threshold.

--- app/src/main/java/com/fb/pearsapplication/Powerset.java
+++ app/src/main/java/com/fb/pearsapplication/Powerset.java
@@ -1,25 +1,22 @@
 package com.fb.pearsapplication;
 
-import com.fb.pearsapplication.models.Group;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Powerset {
-    public static List<List<Group>> subsets(Group[] groups) {
-        List<List<Group>> solution = new ArrayList<>();
-        List<Group> temp_list = new ArrayList<>();
+    public static List<List<String>> subsets(String[] groups) {
+        List<List<String>> solution = new ArrayList<>();
+        List<String> temp_list = new ArrayList<>();
         recursion(solution,temp_list,0,groups);
         return solution;
     }
 
-    public static void recursion(List<List<Group>> solution, List<Group> temp_list, int index, Group[] groups){
+    public static void recursion(List<List<String>> solution, List<String> temp_list, int index, String[] groups){
         if(temp_list.size() > groups.length){
             return;
         }
         solution.add(new ArrayList<>(temp_list));
         for(int i=index; i<groups.length;i++){
-            System.out.println("a");
             temp_list.add(groups[i]);
             recursion(solution, temp_list, i+1, groups);
             temp_list.remove(temp_list.size()-1);
--- app/src/main/java/com/fb/pearsapplication/apioriAlgorithm.java
+++ app/src/main/java/com/fb/pearsapplication/apioriAlgorithm.java
@@ -1,13 +1,6 @@
 package com.fb.pearsapplication;
 
-import android.content.Context;
-
-import com.fb.pearsapplication.models.Group;
-import com.fb.pearsapplication.models.Hobby;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -17,112 +10,123 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 //import org.json.simple.JSONObject;
 
 
 public class apioriAlgorithm {
-    static int size;
-    private Context context;
+    static JSONObject hobbyOccurences = new JSONObject();
 
     /*
         curl -X GET \-H "X-Parse-Application-Id: fbu-pears" \ -H "X-Parse-Master-Key: AngelicaAarushiJaleel" \http://fbu-pears.herokuapp.com/parse/classes/Group
+                curl -X GET \-H "X-Parse-Application-Id: fbu-pears" \ -H "X-Parse-Master-Key: AngelicaAarushiJaleel" \http://fbu-pears.herokuapp.com/parse/classes/_User
+curl -X GET \-H "X-Parse-Application-Id: fbu-pears" \ -H "X-Parse-Master-Key: AngelicaAarushiJaleel" \http://fbu-pears.herokuapp.com/parse/classes/GroupUserRelation
+
+
                 curl -X GET \-H "X-Parse-Application-Id: fbu-pears" \ -H "X-Parse-Master-Key: AngelicaAarushiJaleel" \http://fbu-pears.herokuapp.com/parse/classes/Message
 
 
         --output JSONObject.txt
+
+        // allows cast: System.out.println(obj.get("objectId") instanceof String);
 */
 
-    public static void populatingHobbies(JSONObject jsonObj){
-
-    }
-    public static void populatingHobbies() {
-        System.out.println("Initiating populating hobbies");
-        final ParseQuery<Group> groupQuery = new ParseQuery<Group>(Group.class);
-        System.out.println("Parsed groups");
-        try {
-            groupQuery.find();
-            System.out.println("no error!");
-        } catch (ParseException e) {
-            System.out.println("error!");
-            e.printStackTrace();
+    public static String[] arrGroupObjIds(JSONObject jsonObj) {
+        org.json.simple.JSONArray jsonArray = convertJSONObjToArr(jsonObj);
+        int JSONArrSize = jsonArray.size();
+        String[] objectIds = new String[JSONArrSize];
+        for (int ele = 0; ele < JSONArrSize; ele++) {
+            JSONObject obj = convertObjtoJSONObj(jsonArray.get(ele));
+            String id =  (String)obj.get("objectId");
+            objectIds[ele] = id;
 
         }
-/*        groups.findInBackground(new FindCallback<Group>() {
-            public void done(List<Group> objects, ParseException e) {
-                System.out.println("Initiating done");
-                if (e==null){
-                    System.out.println("e equals null");
-                    Group[] groups = new Group[objects.size()];
-                    List<List<Group>> groupSubsets = Powerset.subsets(objects.toArray(groups));
-                    for (int i = 0; i < groupSubsets.size(); i++) {
-                        ArrayList<Group> subset = (ArrayList) groupSubsets.get(i);
-                        if (subset.size()>10){
-                            System.out.println("this passed"+subset);
-                            Hobby newHobby = new Hobby();
-                            newHobby.newSubset(subset);
-                            countingHobbies(newHobby, subset);
-                            calculatingThreshold(newHobby, -1);
-                            newHobby.saveInBackground(new SaveCallback() {
-                                @Override
-                                public void done(ParseException e) {
-                                    if(e!=null){
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-                else{
-                    System.out.println("e does not equal null");
-                    e.printStackTrace();
-                }
+        return objectIds;
+    }
+
+    public static  Tuple[] arrGroupUserRelations(JSONObject jsonObj){ // array of Tuples (user id, group id)
+        org.json.simple.JSONArray jsonArray = convertJSONObjToArr(jsonObj);
+        int JSONArrSize = jsonArray.size();
+        Tuple[] userGroup = new Tuple[JSONArrSize];
+        for (int ele = 0; ele < JSONArrSize; ele++) {
+            JSONObject obj = convertObjtoJSONObj(jsonArray.get(ele));
+            JSONObject user = convertObjtoJSONObj(obj.get("user"));
+            JSONObject group = convertObjtoJSONObj(obj.get("group"));
+            String group_id = (String) group.get("objectId");
+            String user_id = (String) user.get("objectId");
+            Tuple tuples = new Tuple(user_id, group_id);
+            userGroup[ele] = tuples;
+
+        }
+        return userGroup;
+    }
+
+    public static List<List<String>> gettingPowersets(String[] objectIds) {
+        List<List<String>> groupIdsSubsets = Powerset.subsets((objectIds));
+        return groupIdsSubsets;
+    }
+
+    public static  Map<String, ArrayList> hashMapUserGroup(Tuple[] allTuples){
+        Map<String, ArrayList>  userGroupMap = new HashMap<String, ArrayList>();
+        for (int ug = 0 ; ug<allTuples.length; ug++){
+            Tuple current_tuple = allTuples[ug];
+            String current_user_id = current_tuple.getX();
+            if (userGroupMap.containsKey(current_user_id)){
+                ArrayList current_value =  userGroupMap.get(current_user_id);
+                current_value.add(current_tuple.getY());
+                userGroupMap.put(current_tuple.getX(), current_value);
             }
-        });*/
-    }
-
-    public static void countingHobbies(Hobby hobby, ArrayList<Group> subset){
-        int occurences_count=0;
-        for (int group = 0 ; group<subset.size() ;group++){
-            occurences_count = subset.get(group).getUsers().size();
-        }
-        System.out.println(subset+ " : "+ occurences_count);
-        hobby.setOccurences(occurences_count);
-        //do we need to save in background twice?
-
-    }
-
-    public static void setSize(int s){
-        size= s;
-    }
-
-    public static int getHobbiesSize(){
-        final ParseQuery<Hobby> hobbies = new ParseQuery<Hobby>(Hobby.class);
-        hobbies.findInBackground(new FindCallback<Hobby>() {
-            @Override
-            public void done(List<Hobby> objects, ParseException e) {
-                setSize(objects.size());
+            else{
+                ArrayList newMapKeyValue = new ArrayList();
+                newMapKeyValue.add(current_tuple.getY());
+                userGroupMap.put(current_tuple.getX(), newMapKeyValue);
             }
 
-        });
-        System.out.println("current size"+ " : "+ size);
-        return size;
+        }
+        return userGroupMap;
     }
 
-    public static void calculatingThreshold(Hobby hobby, final int minThreshold){ // min threshold in percentage form
-        double hobbyThreshold= (hobby.getOccurences()/getHobbiesSize())*100;
-        System.out.println(hobby.getSubsets()+ " threshold : "+  hobbyThreshold);
-        if (hobbyThreshold< minThreshold){
-            hobby.setThreshold(-1); // you do not want this!
+    public static ArrayList updatingJSON(List<String> powerset, Map<String, ArrayList>  userGroupMap) { //how many times this set occurs , if it contains both object ids then count it
+        int occurences_count = 0;
+        boolean containsAllGroups;
+        Set key_set = userGroupMap.keySet();
+        ArrayList<String> arr_key_set = new ArrayList<>();
+        arr_key_set.addAll(key_set);
+        for (int user = 0; user < userGroupMap.size(); user++) {
+            ArrayList tuple_current_groups = userGroupMap.get(arr_key_set.get(user));
+            containsAllGroups = true;
+            for (int powersetGroup =0 ; powersetGroup< powerset.size(); powersetGroup++){
+                if (!tuple_current_groups.contains(powerset.get(powersetGroup))){
+                    containsAllGroups = false;
+                }
+            }
+            if (containsAllGroups){
+                occurences_count +=1;
+            }
+
         }
-        else{
-            hobby.setThreshold(hobby.getOccurences()/size);
-        }
+        double threshold = calculatingThreshold(occurences_count, 0, userGroupMap);
+        ArrayList poweroccurthresh = new ArrayList();
+        poweroccurthresh.add(powerset);
+        poweroccurthresh.add(occurences_count);
+        poweroccurthresh.add(threshold);
+       return poweroccurthresh;
     }
-    public JSONObject fileToJSON(String user, String filename){
-        String filePath = "/Users/"+user+"/AndroidStudioProjects/pearsApplication/app/src/main/assets/"+filename;
+
+    public static double calculatingThreshold(int occurences, final int minThreshold, Map<String, ArrayList> userGroupMap) { // min threshold in percentage form
+        double hobbyThreshold = (occurences / userGroupMap.size()) * 100;
+        if (hobbyThreshold < minThreshold) {
+           hobbyThreshold = -1;
+        }
+        return hobbyThreshold;
+    }
+
+    public JSONObject fileToJSON(java.lang.String user, java.lang.String filename) {
+        java.lang.String filePath = "/Users/" + user + "/AndroidStudioProjects/pearsApplication/app/src/main/assets/" + filename;
         File file = new File(filePath);
 
         BufferedReader br = null;
@@ -132,15 +136,13 @@ public class apioriAlgorithm {
             e.printStackTrace();
         }
 
-        String st;
+        java.lang.String st;
         try {
             while ((st = br.readLine()) != null) {
                 JSONParser parser = new JSONParser();
                 JSONObject json = null;
                 try {
                     json = (JSONObject) parser.parse(st);
-                    System.out.println(json);
-                    //System.out.println(json);
                 } catch (org.json.simple.parser.ParseException e) {
                     e.printStackTrace();
                 }
@@ -152,37 +154,47 @@ public class apioriAlgorithm {
         return null;
 
     }
-    public static org.json.simple.JSONArray convertJSONObjectToArray(JSONObject JSONObj){
-        Object results = JSONObj.get("results");
+
+    public static org.json.simple.JSONArray convertJSONObjToArr(JSONObject JSONObj) {
+        java.lang.Object results = JSONObj.get("results");
         org.json.simple.JSONArray JSONarr = (org.json.simple.JSONArray) results;
-        System.out.println(JSONarr);
         return JSONarr;
 
     }
 
-    public static void main(String args[]){
-
-        apioriAlgorithm a = new apioriAlgorithm();
-       JSONObject lol = a.fileToJSON("jaleelismail","JaleelMessages.json");
-       convertJSONObjectToArray(lol);
+    public static JSONObject convertObjtoJSONObj(java.lang.Object obj) {
+        return (JSONObject) obj;
     }
 
-/**/
-/*    public static void userReccomendations (User user){
-        ArrayList arrayListUser = new ArrayList();
-        arrayListUser.add(user);
-        ParseQuery<Group> groupsQuery = new ParseQuery<Group>(Group.class);
-        groupsQuery.whereContainedIn(Group.KEY_USERS, arrayListUser);
-        groupsQuery.findInBackground(new FindCallback<Group>() {
-            @Override
-            public void done(List<Group> objects, ParseException e) {
-                Group[] groups = new Group[objects.size()];
-                List<List<Group>> groupSubsets = Powerset.subsets(objects.toArray(groups));
+    public static org.json.simple.JSONArray convertObjToJSONArr(java.lang.Object obj) {
+        return (org.json.simple.JSONArray) obj;
+    }
 
-            }
-        });
+    public static JSONObject creatingJSON() {
+        apioriAlgorithm a = new apioriAlgorithm();
+        JSONObject json = a.fileToJSON("angcast", "JSONGroup.json");
+        List<List<String>> powerset= gettingPowersets(arrGroupObjIds(json));
 
-    }*/
+        JSONObject json2 = a.fileToJSON("angcast","GroupUserRelations.json");
+        Tuple[] gur = arrGroupUserRelations(json2);
+        Map<String, ArrayList> map = hashMapUserGroup(gur);
+        JSONArray arr = new JSONArray();
+        for (int subset = 0 ; subset<powerset.size(); subset++){
+            ArrayList subset_info = updatingJSON(powerset.get(subset), map);
+            JSONObject one_object = new JSONObject();
+            one_object.put("powerset", subset_info.get(0));
+            one_object.put("occurences", subset_info.get(1));
+            one_object.put("threshold", subset_info.get(2));
+            arr.add(one_object);
+        }
+        JSONObject final_object = new JSONObject();
+        final_object.put("results", arr);
+        return final_object;
+    }
 
 
+    public static void main(java.lang.String args[]) {
+        System.out.println(creatingJSON());
+
+    }
 }
